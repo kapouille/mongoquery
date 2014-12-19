95ab165c7b354505861c3f831736c77ceae32a5d
Olivier Carrère <ocarrere@cisco.com>
2014-12-19 11:53:49 +0000

    package management files

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+HERE = os.path.dirname(__file__)
+
+def read_file(filename):
+    with open(os.path.join(HERE, filename)) as fh:
+        return fh.read().strip(' \t\n\r')
+
+README = read_file("README.rst")
+
+setup(
+    name='mongoquery',
+    version=read_file("VERSION.txt"),
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Utilities"
+    ],
+    author='Olivier Carrere',
+    description="A utility library that provides a MongoDB-like query "
+                "language for querying python collections. It's mainly "
+                "intended to parse objects structured as fundamental types in "
+                "a similar fashion to what is produced by `JSON` or `YAML` "
+                "parsers.",
+    author_email='olivier.carrere@gmail.com',
+    url='http://github.com/kapouille/mongoquery',
+    keywords='mongodb query match',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False
+)
