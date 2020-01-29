5b2d50a8fab1097cfaa08346bb16e17d007cd41f
Paul Weaver <paul@concertdaw.co.uk>
2020-01-29 12:49:03 +0000

    Fix import of abstract container classes to Python 3.9 compatible module

--- mongoquery/__init__.py
+++ mongoquery/__init__.py
@@ -4,7 +4,7 @@ MongoDB Query Language queries.
 """
 
 import re
-from collections import Sequence, Mapping
+from collections.abc import Sequence, Mapping
 from six import string_types
 
 # pylint: disable=invalid-name
